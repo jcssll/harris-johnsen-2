c394008eb848017c868e5cceb5bb488af29a6850
jcssll <essentialnetworker@gmail.com>
2025-03-12 20:29:40 -0400

    Initial Commit
    Initial Commit for forked repo

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -36,19 +36,19 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
+          Joshua
           <br />
-          Johnsen
+          Cassell
         </h1>
         <h2>Frontend Developer</h2>
         <span>
           <a href="https://twitter.com/" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/jcssll" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/joshua-f-cassell-iii-347aab81/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
